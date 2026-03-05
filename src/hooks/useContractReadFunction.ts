1d440b504003ef93963f24648671ec31770186e7
eclipseeer <eclipseeer@gmail.com>
2026-03-05 11:45:03 +0200

    Add types for useContractReadFunction

--- src/hooks/useContractReadFunction.ts
+++ src/hooks/useContractReadFunction.ts
@@ -1,33 +1,37 @@
 import { useQuery } from '@tanstack/react-query';
-import { type AccountId } from 'near-api-ts';
+import type {
+  UseContractReadFunction,
+  InnerUseContractReadFunctionArgs,
+} from '../../types/hooks/useContractReadFunction.ts';
 import { useNearStore } from '../store/NearStoreProvider.tsx';
 
-type UseContractReadFunctionArgs = {
-  contractAccountId: AccountId;
-  functionName: string;
-  functionArgs?: any; // TODO Fix type
-  query?: {
-    enabled?: boolean;
-  };
-};
-
-export const useContractReadFunction = (args: UseContractReadFunctionArgs) => {
+export const useContractReadFunction: UseContractReadFunction = (
+  args: InnerUseContractReadFunctionArgs,
+): ReturnType<UseContractReadFunction> => {
   const getContext = useNearStore((store) => store.getContext);
   const context = getContext();
 
+  const { query, ...callArgs } = args;
+
   return useQuery({
-    enabled: args.query?.enabled ?? true,
+    enabled: query?.enabled ?? true,
     queryKey: [
       'callContractReadFunction',
-      args.contractAccountId,
-      args.functionName,
+      callArgs.contractAccountId,
+      callArgs.functionName,
+      callArgs.functionArgs,
+      callArgs.withStateAt,
+      callArgs.policies,
     ],
-    queryFn: (queryArgs) =>
+    queryFn: ({ signal }) =>
       context.client.callContractReadFunction({
         contractAccountId: args.contractAccountId,
         functionName: args.functionName,
         functionArgs: args.functionArgs,
-        options: { signal: queryArgs.signal },
+        options: {
+          serializeArgs: args.options?.serializeArgs,
+          signal,
+        },
       }),
   });
 };
